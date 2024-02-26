7bd8e0bb45a8f227e2b6552ea5a28f33628d1b00
Rahmat Deky BC_Batam <rahmat.hidayat98@kemenkeu.go.id>
2024-02-26 15:52:06 +0700

    sampai praktikum ke 3

---
+++ app/Http/Controllers/ListBarangController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ListBarangController extends Controller
+{
+    function tampilkan($id, $nama)
+    {
+        return view('list_barang', [
+            'id' => $id,
+            'nama' => $nama
+        ]);
+    }
+}
